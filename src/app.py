82088e7ac3d98ecda66eb15db8b98191b7d5c7aa
Joselin Escalante <joselinescalante.10@gmail.com>
2023-11-26 20:09:08 -0600

    27-11-2023 MJEM

---
+++ src/app.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, render_template
+
+from flask_mysqldb import MySQL
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    grupo:"IEVN-1002"
+    lista=["IEVN 1001", "IEVN1002", "IEVN1003"]
+    return render_template('index.html' ,grupo=grupo,lista=lista)
+
+@app.route('/hola')
+def hola():
+    return "Saludo UTL"
+
+@app.route('/user/<string:nombre>')
+def user(nombre):
+    return 'Saludos {0}'.format(nombre)
+
+@app.route('/numero/<int:n1>')
+def numero(n1):
+    return 'El número es {}'.format(n1)
+
+@app.route('/user/<int:id>/<string:nom>')
+def user1(id,nom):
+    return 'ID {} Nombre: {}'.format(id,nom)
+
+@app.route('/suma/<float:n1><float:n2>')
+def suma(n1,n2):
+    return 'La suma es {}'.format(n1+n2)
+
+@app.route('/default')
+@app.route('/default<string:dd>')
+def default(dd='Fulanito'):
+    return "<h1>El nombre es: {} </h1".format(dd)
+
+@app.route('/alumnos')
+def alumnos():
+    return render_template('alumnos.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
